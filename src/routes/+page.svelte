7310163160ace2e05fd3ed3c39fd06feecc82f25
grantgariepy <grantgariepy@protonmail.com>
2023-04-25 10:48:43 -0600

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -381,7 +381,7 @@
 										<ol class="mt-6 space-y-4">
 											<li class="flex gap-4">
 												<div
-													class="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0"
+													class="bg-zinc-800 relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0"
 												>
 													<img
 														alt=""
@@ -417,7 +417,7 @@
 											</li>
 											<li class="flex gap-4">
 												<div
-													class="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0"
+													class="bg-zinc-800 relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0"
 												>
 													<img
 														alt=""
