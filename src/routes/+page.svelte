2c06666712a3465d308ce5e860e7d749fc023eb6
grantgariepy <86595691+grantgariepy@users.noreply.github.com>
2023-05-03 12:58:25 -0600

    Update +page.svelte
    change footer link hover color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -469,10 +469,10 @@
 							<div class="mx-auto max-w-2xl lg:max-w-5xl">
 								<div class="flex flex-col items-center justify-between gap-6 sm:flex-row">
 									<div class="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
-										<a class="transition hover:text-red-400 dark:hover:text-teal-400" href="/about"
+										<a class="transition hover:text-red-400 dark:hover:text-red-400" href="/about"
 											>About</a
 										><a
-											class="transition hover:text-red-400 dark:hover:text-teal-400"
+											class="transition hover:text-red-400 dark:hover:text-red-400"
 											href="/projects">Projects</a
 										>
 									</div>
